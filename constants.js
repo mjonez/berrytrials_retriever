24fcbac19806a085e5f42db6dc44e120a580a8ed
Matthew Jones <matthewjones204@gmail.com>
2020-06-08 17:46:20 +0100

    update zones constant

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ module.exports = Object.freeze({
     BERRY_SNAPSHOT_INTERVAL_HOURS: 4,
     ENTERLEAVE_PATH: 'logs/enterleave.log',
     SERVER_LOG_PATH: 'logs/server_log.txt',
-    ZONES: ["easy", "medium", "hard", "slide", "extreme", "trial", "race"],
+    ZONES: ["easy", "medium", "hard", "extreme", "insane", "trial", "race"],
     TOP_RUNS_EXCLUDED_GUIDS: ["8ee4d654e01fa03ef2c0362f38ba8778", "000000001cc2de5d75200aa3b9008471"]
 
 });
